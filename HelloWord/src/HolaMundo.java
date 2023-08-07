8e8a8a12e6c696e712ea63316b538f4e0393e5fc
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-07 08:27:14 -0400

    Respaldo

---
+++ HelloWord/src/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    /**
+     * Esta es la clase bootstrap o de entrada a la las aplicaciones en Java
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+        System.out.println("Que honda wey ");
+        System.out.println("Dajan Medina");
+    }
+
+}
